01fa118baf09494f405535515784456170e8d445
Eduardo M <contact@eduardom.dev>
2023-10-16 13:12:20 -0700

    Small fixes

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,22 +5,6 @@ import prisma from "../../../../db";
 const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn({ user, account, profile, email, credentials }) {
-      const profileData = profile as any;
-      const primsaData = {
-        name: profile.name,
-        firstName: profileData.given_name,
-        lastName: profileData.family_name,
-        email: profileData.email,
-        sunet: profileData.preferred_username,
-        affiliations: (profileData.eduPersonScopedAffiliation as String)
-          .split(" "),
-      };
-
-      return true;
-    },
-  },
   providers: [
     {
       id: "stanford",
@@ -35,6 +19,9 @@ const authOptions: AuthOptions = {
       idToken: true,
       checks: ["pkce", "state"],
       profile(profile) {
+        const affiliations = (profile.eduPersonScopedAffiliation as String).split(
+          " ",
+        );
         return {
           id: profile.sub,
           name: profile.name,
@@ -43,9 +30,7 @@ const authOptions: AuthOptions = {
           sunet: profile.preferred_username,
           email: profile.email,
           emailVerified: true,
-          affiliations: (profile.eduPersonScopedAffiliation as String).split(
-            " ",
-          ),
+          affiliations: affiliations,
         };
       },
       userinfo: {
