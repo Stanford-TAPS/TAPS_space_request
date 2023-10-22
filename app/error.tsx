a938b5d412c60e5221ab153c9a99e9590c68fb94
Eduardo M <contact@eduardom.dev>
2023-10-22 14:00:40 -0700

    Improve error component design

--- app/error.tsx
+++ app/error.tsx
@@ -1,6 +1,9 @@
 "use client"; // Error components must be Client Components
 
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect } from "react";
+import { PrimaryButton } from "./components/buttons";
 
 export default function Error({ error, reset }) {
   useEffect(() => {
@@ -8,19 +11,17 @@ export default function Error({ error, reset }) {
     console.error(error);
   }, [error]);
 
-  console.log(error);
 
-  return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button
-        onClick={
-          // Attempt to recover by trying to re-render the segment
-          () => reset()
-        }
-      >
-        Try again
-      </button>
-    </div>
-  );
+
+  return <div className="flex flex-col items-center justify-center h-full">
+    <FontAwesomeIcon icon={faExclamationCircle} className="pb-4 text-6xl text-gray-500 dark:text-neutral-700" />
+    <h1 className="pb-4 text-4xl">Error</h1>
+
+    <p className="text-xl font-roboto">There was an issue Loading this page.</p>
+    <p className="p-4 text-xl font-roboto">Please Try Again or come back later.</p>
+
+    <PrimaryButton href="#" text="Try Again" compact={false} onClick={reset()} />
+
+  </div>;
+
 }
